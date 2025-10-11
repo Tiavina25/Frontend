6635f7ae500ec66998d094bf4d83c7c2893a02b6
Tiavina25 <tiavinaarakotonirina@gmail.com>
2025-10-11 17:31:12 +0300

    Update vite.config.js
    update vite config

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://resto.infinityfreeapp.com/api',
         changeOrigin: true,
       },
     },
